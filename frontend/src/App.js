c7e24086aa150c17a0ff9145fd077b99514bf310
Travis <ga86yam@mytum.de>
2019-11-23 02:16:58 +0100

    Add component folder; Isolate navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="App-MainContainer">
-        hello
       </div>
-      <div className="App-NavBar" />
+      <NavBar />
     </div>
   );
 }
