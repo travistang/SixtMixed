d8d3dd9d27052289c04a1da2c4d2366fb6af96c6
Daniel Bode <daniel@w207-3g-v4.eduroam.dynamic.rbg.tum.de>
2019-11-23 15:00:10 +0100

    use flexbox

--- frontend/src/components/NextEvent.js
+++ frontend/src/components/NextEvent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function NextEvent() {
-    const [nextEvent] = React.useState({startingTime: 16, duration: 3, endTime: 19, title: "Dinner", location:"Boltzmannstraße 3, 85748 Garching"});
+    const [nextEvent] = React.useState({startingTime: 16, duration: 2, endTime: 18, title: "Dinner", location:"Boltzmannstraße 3, 85748 Garching"});
     const [timeSlots, setTimeSlots] = React.useState([]);
 
     React.useEffect( () => {
@@ -12,14 +12,6 @@ export default function NextEvent() {
             setTimeSlots(tmp);
         }, [nextEvent.duration, nextEvent.startingTime, nextEvent.endTime]
     )
-
-    function getHeight() {
-        return ((1/(nextEvent.duration + 2))*100).toString() + "%";
-    }
-
-    function getHeightRest() {
-        return ((1-2/(nextEvent.duration + 2))*100).toString() + "%";
-    }
 
     return(
         <>
@@ -35,12 +27,12 @@ export default function NextEvent() {
                 <div className="top-border">{(nextEvent.endTime + 1).toString()+":00"}</div>
             </div>
             <div id="event">
-                <div style={{height: getHeight()}}></div>
-                <div className="eventSlot" style={{height: getHeightRest()}}>
-                    <p style={{'font-weight': 'bold'}}>{nextEvent.title}</p>
+                <div style={{flex: 1}}></div>
+                <div className="eventSlot" style={{flex:nextEvent.duration}}>
+                    <p className="title">{nextEvent.title}</p>
                     <p className="location">{nextEvent.location}</p>
                 </div>
-                <div style={{height: getHeight()}}></div>
+                <div style={{flex: 1}}></div>
             </div>
         </div>
         </>
