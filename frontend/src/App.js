46479cd41068a3e90fda64c3cfc5c87a79950a5e
Travis <ga86yam@mytum.de>
2019-11-23 02:13:43 +0100

    add css import in index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="App-MainContainer">
-        hello :D:D
+        hello
       </div>
       <div className="App-NavBar" />
     </div>
