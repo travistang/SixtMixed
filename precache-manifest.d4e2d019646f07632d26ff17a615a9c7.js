8360b23ed61da3ee6d5b809a3a2da9093b21f586
Travis <ga86yam@mytum.de>
2019-11-24 10:26:56 +0100

    Updates

---
+++ precache-manifest.d4e2d019646f07632d26ff17a615a9c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b3a3abeac1ff62a1666725fb1e7c09",
+    "url": "/SixtMixed/index.html"
+  },
+  {
+    "revision": "f49f35ae23200727b61b",
+    "url": "/SixtMixed/static/css/main.7e579839.chunk.css"
+  },
+  {
+    "revision": "55fff682ce8e71478b70",
+    "url": "/SixtMixed/static/js/2.dbc4a316.chunk.js"
+  },
+  {
+    "revision": "f49f35ae23200727b61b",
+    "url": "/SixtMixed/static/js/main.4359aa05.chunk.js"
+  },
+  {
+    "revision": "fa966bd6541f48fe02e7",
+    "url": "/SixtMixed/static/js/runtime-main.d6621ef9.js"
+  }
+]);
