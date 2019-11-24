cefcb987c49ab4f8563c5b22f23598b31518561d
Travis <ga86yam@mytum.de>
2019-11-24 01:34:19 +0100

    update date

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -32,12 +32,12 @@ export const offers2 = [
             totalPrice: 12.99,
             isTaxi: false,
             isDriver: true,
-            numberFellowPassengers: 1,
+            numberFellowPassengers: 0,
             isShared: true,
         },
         {
-            startTime: getDateOfHours(11, 30),
-            endTime: getDateOfHours(11, 50),
+            startTime: getDateOfHours(11, 20),
+            endTime: getDateOfHours(11, 30),
             startLocation: "Giesing",
             startLocationCoordinate: { 
                 lat: 48.110904,
@@ -48,11 +48,11 @@ export const offers2 = [
                 lat: 48.26504,
                 lng: 11.6693806
             },
-            totalPrice: 10.50,
-            isTaxi: false,
-            isDriver: true,
-            numberFellowPassengers: 2,
-            isShared: true,
+            totalPrice: 30.25,
+            isTaxi: true,
+            isDriver: false,
+            numberFellowPassengers: 0,
+            isShared: false,
         },
     ]
 ]
@@ -113,10 +113,10 @@ export const offers3 = [
 //ontime, with taxi
 export const events = [
     {
-        startTime: getDateOfHours(18, 10), 
-        endTime: getDateOfHours(19, 30),
-        title: "Dinner", 
-        location:"Augustiner-Keller, Arnulfstraße 52, 80335 München",
+        startTime: getDateOfHours(11, 45), 
+        endTime: getDateOfHours(13, 45),
+        title: "DS Lecture", 
+        location:"Boltzmannstraße 3, 85748 Garching",
     },
     ...events2,
     ...events3
@@ -125,42 +125,42 @@ export const events = [
 export const offers = [
     [
         {
-            startTime: getDateOfHours(17, 56),
-            endTime: getDateOfHours(18, 7),
+            startTime: getDateOfHours(11, 10),
+            endTime: getDateOfHours(11, 20),
             startLocation: "Bavarian state library, Ludwigstraße 16, 80539 München",
             startLocationCoordinate: { 
                 lat: 48.1474093,
                 lng: 11.578423,
             },
-            destination: "Augustiner-Keller, Arnulfstraße 52, 80335 München",
+            destination: "Boltzmannstraße 3, 85748 Garching",
             destinationCoordinate: {
-                lat: 48.1435227,
-                lng: 11.5492731,
+                lat: 48.26504,
+                lng: 11.6693806
             },
-            totalPrice: 16.46,
-            isTaxi: true,
-            isDriver: false,
-            numberFellowPassengers: 3,
-            isShared: true,
-        },
-        {
-            startTime: getDateOfHours(17, 51),
-            endTime: getDateOfHours(18, 5),
-            startLocation: "Bavarian state library, Ludwigstraße 16, 80539 München",
-            startLocationCoordinate: { 
-                lat: 48.1474093,
-                lng: 11.578423,
-            },
-            destination: "Augustiner-Keller, Arnulfstraße 52, 80335 München",
-            destinationCoordinate: {
-                lat: 48.1435227,
-                lng: 11.5492731,
-            },
-            totalPrice: 18.29,
-            isTaxi: true,
+            totalPrice: 8.00,
+            isTaxi: false,
             isDriver: false,
             numberFellowPassengers: 1,
             isShared: true,
+        },
+        {
+            startTime: getDateOfHours(11, 10),
+            endTime: getDateOfHours(11, 20),
+            startLocation: "Bavarian state library, Ludwigstraße 16, 80539 München",
+            startLocationCoordinate: { 
+                lat: 48.1474093,
+                lng: 11.578423,
+            },
+            destination: "Boltzmannstraße 3, 85748 Garching",
+            destinationCoordinate: {
+                lat: 48.26504,
+                lng: 11.6693806
+            },
+            totalPrice: 11.40,
+            isTaxi: false,
+            isDriver: true,
+            numberFellowPassengers: 0,
+            isShared: false,
         },
     ], ...offers2, ...offers3
 ]
